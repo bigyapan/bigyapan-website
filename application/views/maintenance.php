9e3870f5b3b63baf11c900f7d31f6c1cd760ee14
aj33b <er.ajeebrimal@gmail.com>
2022-06-15 16:51:43 +0545

    Maintenance Background Changed

--- application/views/maintenance.php
+++ application/views/maintenance.php
@@ -98,15 +98,15 @@
 </head>
 <body>
 
-<div class="maintenance">
-    <div class="maintenance-inner">
-        <div class="content">
-            <h1 class="site-title"><?php echo $this->general_settings->application_name; ?></h1>
-            <h2 class="title"><?php echo $this->general_settings->maintenance_mode_title; ?></h2>
-            <p class="description"><?php echo $this->general_settings->maintenance_mode_description; ?></p>
-        </div>
-    </div>
-</div>
+<!--<div class="maintenance">-->
+<!--    <div class="maintenance-inner">-->
+<!--        <div class="content">-->
+<!--            <h1 class="site-title">--><?php //echo $this->general_settings->application_name; ?><!--</h1>-->
+<!--            <h2 class="title">--><?php //echo $this->general_settings->maintenance_mode_title; ?><!--</h2>-->
+<!--            <p class="description">--><?php //echo $this->general_settings->maintenance_mode_description; ?><!--</p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
 
 </body>
 </html>
